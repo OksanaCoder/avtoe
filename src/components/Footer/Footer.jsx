b74e2d7baba2d8beafe5a84fad5428ef910318af
Ksena <ksena@MacBook-Pro.local>
2021-02-03 02:05:29 +0200

    feat(app): adding google maps

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,9 +6,49 @@ import home from '../../img/home.svg'
 import facebook from '../../img/facebook.svg'
 import instagram from '../../img/instagram.svg'
 import './style.css'
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
 
 
 const Footer = () => {
+    const containerStyle = {
+        width: '100%',
+        height: '400px',
+        marginTop: '40px'
+      };
+      
+      const center = {
+        lat: 50.48970142061635,
+        lng: 30.488861167016232
+      };
+
+      const locations = [
+          {
+              name: 'avtoe',
+              location: {
+                lat: 50.48970142061635,
+                lng: 30.488861167016232
+              }
+          }
+      ]
+      
+    // const { isLoaded } = useJsApiLoader({
+    //     id: 'google-map-script',
+    //     googleMapsApiKey: "AIzaSyABS95c2rEoEhNOc-7nFFD9kf3MokK582w"
+    //   })
+    
+    //   const [map, setMap] = React.useState(null)
+    
+    //   const onLoad = React.useCallback(function callback(map) {
+    //     const bounds = new window.google.maps.LatLngBounds();
+    //     map.fitBounds(bounds);
+    //     setMap(map)
+    //   }, [])
+    
+    //   const onUnmount = React.useCallback(function callback(map) {
+    //     setMap(null)
+    //   }, [])
+
     return (
         <div style={{background: '#222833', padding: '100px 50px 20px 50px'}}>
             <Container fluid>
@@ -28,7 +68,6 @@ const Footer = () => {
                            
                         </ul>
                       </div>
-
                     </Col>
                     <Col lg={4} md={4} sm={12} className='text-white footer-item font-footer'>
                          <img src={mail} className='icon-fill'/>
@@ -43,6 +82,27 @@ const Footer = () => {
                             <h6>Адреса:</h6>
                             <small className='font-footer'>г. Киев, проспект Степана Бандеры, 13</small>
                           </div>
+                        
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className='opacity'>
+                    <LoadScript
+                           googleMapsApiKey='AIzaSyABS95c2rEoEhNOc-7nFFD9kf3MokK582w'>
+                            <GoogleMap
+                            mapContainerStyle={containerStyle}
+                            zoom={18}
+                            center={center}
+                            >
+                           {
+                                center.lat &&
+                                ( 
+                                <Marker position={center} />
+                                ) 
+                            }
+                         </GoogleMap>
+                        </LoadScript>
+                    
                     </Col>
                 </Row>
                 <Row className='line mt-5'>
@@ -67,5 +127,7 @@ const Footer = () => {
         </div>
     )
 }
+
+
 
 export default Footer
--- src/components/MobApp/MobApp.jsx
+++ src/components/MobApp/MobApp.jsx
@@ -8,17 +8,20 @@ import './style.css'
 
 const MobApp = () => {
     return (
-        <div className='grey-back'>
+        <div className='yellow-back'>
             <Row className='center-app'>
                 <Col lg={6} md={6} sm={12}>
                     <img src={image} className='img-app'/>
                 </Col>
                 <Col lg={6} md={6} sm={12} className='text-center'>
-                    <h5>Завантажуй Мобільний Додаток.</h5>
-                    <p>Будь в курсі останніх оновлень.</p>
-                    <Row style={{margin: '0'}}>
+                    <h3 style={{fontWeight: '700', marginTop: '20px'}}>Завантажуй Наш Мобільний Додаток</h3>
+                    <p style={{letterSpacing: '2px'}}>Будь в курсі останніх оновлень.</p>
+                    <Row className='align-center'>
                         <Col>
                             <img src={ios} width='150px' height='150px'/>
+                            
+                       </Col>
+                       <Col>
                             <img src={android} width='170px' height='75px'/>
                        </Col>
                     </Row>
