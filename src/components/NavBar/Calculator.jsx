0ff1fa694a15dc1baddc16efb25b330449a1b86a
Ksena <ksena@MacBook-Pro.local>
2021-02-10 18:31:23 +0200

    feat(app): calculator

--- src/components/NavBar/Calculator.jsx
+++ src/components/NavBar/Calculator.jsx
@@ -9,10 +9,65 @@ const Calculator = (props) => {
     
    const [show_calc, setShow_calc] = useState(false)
    const handleClose_calc = () => setShow_calc(false);
-
+   const [userValues, setUserValues] = useState({
+       amount: 0,
+       interest: 0,
+       years: 0
+   })
+  
     useEffect(() => {
         setShow_calc(props);
       }, [props]);
+      const [results, setResults] = useState({
+        monthlyPayment: '',
+        totalPayment: '',
+        totalInterest: '',
+        isResult: false
+    })
+      const handleChange = (event) => {
+            setUserValues({...userValues, [event.target.name]: event.target.value})
+           
+      }
+
+     const calculateResults = ({amount, interest, years}) => {
+        const calculatedInterest = interest / 100 / 12
+        const calculatedPayments = years * 12
+        const monthly = amount * calculatedInterest;
+
+        if (isFinite(monthly)) {
+            const monthlyPaymentCalculated = monthly.toFixed(2);
+            const totalPaymentCalculated = (monthly * calculatedPayments).toFixed(2);
+            const totalInterestCalculated = (monthly * calculatedPayments - amount).toFixed(2);
+       
+            // Set up results to the state to be displayed to the user
+            setResults({
+              monthlyPayment: monthlyPaymentCalculated,
+              totalPayment: totalPaymentCalculated,
+              totalInterest: totalInterestCalculated,
+              isResult: true,
+            });
+          }
+          return;
+     }
+     
+     let handleSubmit = (e) => {
+        e.preventDefault()
+        calculateResults(userValues)
+    }
+    const clearFields = () => {
+        setUserValues({
+          amount: '',
+          interest: '',
+          years: '',
+        });
+     
+        setResults({
+          monthlyPayment: '',
+          totalPayment: '',
+          totalInterest: '',
+          isResult: false,
+        });
+      };
     return (
         <>
          <Modal show={show_calc} onHide={handleClose_calc} className='login-form calc_form'>
@@ -25,40 +80,40 @@ const Calculator = (props) => {
                      <Row>
                         <Col lg={6} md={6} sm={12}>
                               <Form.Label htmlFor="inlineFormInputGroup" >
-                              Ціна
+                              Ціна, $
                               </Form.Label>
                               <InputGroup className="mb-2">
                               <InputGroup.Prepend>
-                                 <InputGroup.Text><img src={price} className='icon-calc'/></InputGroup.Text>
+                                 <InputGroup.Text ><img src={price} className='icon-calc'/></InputGroup.Text>
                               </InputGroup.Prepend>
-                              <FormControl id="inlineFormInputGroup"  />
+                              <FormControl type='number' name='amount' value={userValues.amount} onChange={handleChange} id="inlineFormInputGroup"  />
                               </InputGroup>
                         </Col>
                         <Col lg={6} md={6} sm={12}>
                               <Form.Label htmlFor="inlineFormInputGroup" >
-                              Ставка, %
+                              Ставка, %  
                               </Form.Label>
                               <InputGroup className="mb-2">
                               <InputGroup.Prepend>
                                  <InputGroup.Text><img src={percent} className='icon-calc'/></InputGroup.Text>
                               </InputGroup.Prepend>
-                              <FormControl id="inlineFormInputGroup" />
+                              <FormControl  type='number' name='interest' value={userValues.interest} onChange={handleChange} id="inlineFormInputGroup" />
                               </InputGroup>
                         </Col>
                      </Row>
                      <Row className='line-bottom mt-4'>
-                        <Col lg={6} md={6} sm={12}>
+                        <Col lg={12} md={12} sm={12}>
                               <Form.Label htmlFor="inlineFormInputGroup" >
                               Період (місяців)
                               </Form.Label>
                               <InputGroup className="mb-2">
                               <InputGroup.Prepend>
-                                 <InputGroup.Text><img src={calendar} className='icon-calc'/></InputGroup.Text>
+                                 <InputGroup.Text ><img src={calendar} className='icon-calc'/></InputGroup.Text>
                               </InputGroup.Prepend>
-                              <FormControl id="inlineFormInputGroup"  />
+                              <FormControl name='years' value={userValues.months} onChange={handleChange} id="inlineFormInputGroup"  />
                               </InputGroup>
                         </Col>
-                        <Col lg={6} md={6} sm={12}>
+                        {/* <Col lg={6} md={6} sm={12}>
                               <Form.Label htmlFor="inlineFormInputGroup" >
                               Завдаток
                               </Form.Label>
@@ -68,33 +123,46 @@ const Calculator = (props) => {
                               </InputGroup.Prepend>
                               <FormControl id="inlineFormInputGroup"  />
                               </InputGroup>
-                        </Col>
+                        </Col> */}
                      </Row>
+                  
                     
                        <Row className='calc-output'>
-                          <Col lg={4} md={12} sm={12} className='border-right-calc'>
-                             <h5>Місячний платіж</h5>
-                             <h4>-</h4>
+                          <Col lg={6} md={12} sm={12} className='border-right-calc'>
+                             <h5>Місячний платіж, $</h5>
+                             <h4>{results.monthlyPayment}</h4>
                           </Col>
-                          <Col lg={4} md={12} sm={12} className='border-right-calc'>
-                             <h5>Загальний відсоток</h5>
-                             <h4>-</h4>
-                          </Col>
-                          <Col lg={4} md={12} sm={12}>
-                             <h5>Загальна сума</h5>
-                             <h4>-</h4>
+                          {/* <Col lg={4} md={12} sm={12} className='border-right-calc'>
+                             <h5>Загальний відсоток, %</h5>
+                             <h4>{results.totalInterest}</h4>
+                          </Col> */}
+                          <Col lg={6} md={12} sm={12}>
+                             <h5>Загальна сума, $</h5>
+                             <h4>{results.totalPayment}</h4>
                           </Col>
                        </Row>
                      
-                       <div className='text-right mt-5'>
-                     
+                    <Row className='mt-5 text-center'>
+                     <Col lg={4} md={12} sm={12}>
+                       <Button variant="primary" type="submit" className='btn-form yellow-back mb-2' onClick={handleSubmit}>
+                          Порахувати
+                        </Button>
+                    </Col>
+                    <Col lg={4} md={12} sm={12}>
+                       <Button variant="primary" className='btn-form grey-back ml-2 mb-2' onClick={clearFields}>
+                          Очистити
+                        </Button>
+                    </Col>
+                    <Col lg={4} md={12} sm={12}>
                         <Button variant="primary" type="submit" className='btn-form grey-back' onClick={handleClose_calc}>
                            Скасувати
                         </Button>
-                       </div>
+                    </Col>
+                    
+                    </Row>
                   </Form>
-                     </Modal.Body>
-                     </Modal>
+               </Modal.Body>
+            </Modal>
         </>
     )
 }
