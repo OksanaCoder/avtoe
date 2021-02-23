7614361762f962d7e8504774cc20549557b6b4e9
Emmanuel Adedeji <emmans4destiny@gmail.com>
2021-02-23 17:22:14 +0200

    user login done!

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,179 +1,196 @@
-import React, { useEffect, useState,useRef }  from 'react'
-import { Modal, Button, Form } from 'react-bootstrap';
-import moment from 'moment';
-import axios from 'axios';
-
+import React, {  useState } from "react";
+import { Modal, Button, Form } from "react-bootstrap";
+import moment from "moment";
+import axios from "axios";
+import { Redirect } from 'react-router-dom';
 const SignUpForm = (props) => {
-   
-  const form = useRef();
-  const checkBtn = useRef();
-  const [user, setUser] = useState({
-     firstname:"",
-     lastname:"", 
-     username:"",
-     email:"",
-     password:"", 
-     dob:"",
-     phone:""
-  })
+   console.log(props)
   const [firstname, setFirstname] = useState("");
   const [lastname, setLastname] = useState("");
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [dob, setDob] = useState("");
-  const [phone, setPhone]=useState("");
-  const [successful, setSuccessful] = useState(false);
+  const [phone, setPhone] = useState("");
 
   const API_URL = process.env.REACT_APP_URL;
 
-const register = (firstname, lastname,username, email, password,dob,phone) => {
-  return axios.post(API_URL + "/users/register", {
-   firstname, lastname,
+  const register = (
+    firstname,
+    lastname,
     username,
     email,
     password,
-    dob,phone
-  });
+    dob,
+    phone
+  ) => {
+    return axios.post(API_URL + "/users/register", {
+      firstname,
+      lastname,
+      username,
+      email,
+      password,
+      dob,
+      phone,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const succesregv = await register(
+      firstname,
+      lastname,
+      username,
+      email,
+      password,
+      dob,
+      phone
+    );
+    //form.current.validateAll()
+    if (succesregv) {
+      alert("Registered Successfully");
+      //props.history.push("/");
+      return <Redirect to="/" />;
+
+    } else {
+      console.log("please check login");
+    }
+  };
+  const onChangeFirstname = (e) => {
+    const firstname = e.target.value;
+    setFirstname(firstname);
+  };
+
+  const onChangeLastname = (e) => {
+    const lastname = e.target.value;
+    setLastname(lastname);
+  };
+
+  const onChangeUsername = (e) => {
+    const username = e.target.value;
+    setUsername(username);
+  };
+  const onChangeEmail = (e) => {
+    const email = e.target.value;
+    setEmail(email);
+  };
+
+  const onChangePassword = (e) => {
+    const password = e.target.value;
+    setPassword(password);
+  };
+
+  const onChangeDob = (e) => {
+    const dob = e.target.value;
+    setDob(dob);
+  };
+  const onChangePhone = (e) => {
+    const phone = e.target.value;
+    setPhone(phone);
+  };
+
+  return (
+    <>
+      <Modal
+        show={props.show1}
+        onHide={props.handleClose1}
+        className="login-form"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Реєстрація</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formBasicFirstName">
+              <Form.Control
+                type="text"
+                placeholder="Firs tname"
+                value={firstname}
+                onChange={onChangeFirstname}
+                name="firstname"
+              />
+            </Form.Group>
+            <Form.Group controlId="formBasicLastName">
+              <Form.Control
+                type="text"
+                placeholder="Last name"
+                value={lastname}
+                onChange={onChangeLastname}
+                name="lastname"
+              />
+            </Form.Group>
+            <Form.Group controlId="formBasicName">
+              <Form.Control
+                type="text"
+                placeholder="User name"
+                value={username}
+                onChange={onChangeUsername}
+                name="username"
+              />
+            </Form.Group>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Control
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={onChangeEmail}
+                name="email"
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Control
+                type="password"
+                placeholder="Пароль"
+                value={password}
+                onChange={onChangePassword}
+                name="password"
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicDateOfBirth">
+              <Form.Control
+                type="date"
+                value={moment(dob).format("YYYY-MM-DD")}
+                onChange={onChangeDob}
+                name="dob"
+              />
+            </Form.Group>
+            <Form.Group controlId="formBasicPhone">
+              <Form.Control
+                placeholder="Номер телефону"
+                value={phone}
+                onChange={onChangePhone}
+                name="phone"
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check
+                type="checkbox"
+                label="Я погоджуюся з умовами користування сайтом"
+              />
+            </Form.Group>
+            <div className="flex-form mt-5">
+              <Button
+                variant="primary"
+                type="submit"
+                className="btn-form yellow-back"
+              >
+                Зареєструватись
+              </Button>
+              <Button
+                variant="primary"
+                className="btn-form grey-back"
+                onClick={props.handleClose1}
+              >
+                Скасувати
+              </Button>
+            </div>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
 };
-   const handleSubmit = async (e) => {
-      e.preventDefault()
-      const succesregv= await register(firstname, lastname,username, email, password,dob,phone);
-      //form.current.validateAll()
-      if(succesregv){
-         alert("Registered Successfully")
-      }else{
-         console.log("please check login")
-      }
-      // if(checkBtn.current.context._errors.length ===0){
-      //   const succesregv= await register(firstname, lastname,username, email, password,dob,phone);
-      //   if(succesregv){
-      //      alert("Registered Successfully")
-      //   }else{
-      //      console.log("please check login")
-      //   }
-      // }
-      
-      // fetch('https://avtoe-back.herokuapp.com/users/register', {
-      //    method: 'POST',
-      //    headers: {
-      //       'Content-Type' : 'application/json'
-      //    },
-      //    body: JSON.stringify(user)
-      // })
-      // .then(data => data.json())
-      // .then((res) => {
-      //    console.log(res)
-      //    setUser('')
-      // })
-      // .catch((err) => console.log('error'))
-   //    axios({
-   //       method: 'post',
-   //       url: 'https://avtoe-back.herokuapp.com/users/register',
-   //       data: user,
-   //       headers: {'Content-Type': 'multipart/form-data' }
-   //       })
-   //       .then(function (response) {
-   //           //handle success
-   //           //setUser('')
-   //           console.log(response);
-             
-   //       })
-   //       .catch(function (response) {
-   //           //handle error
-   //           console.log(response);
-   //       });
-   // }
-   }
-   const onChangeFirstname = (e) => {
-      const firstname= e.target.value;
-      setFirstname(firstname);
-    };
-  
-    const onChangeLastname = (e) => {
-      const lastname= e.target.value;
-      setLastname(lastname);
-    };
-  
-    const onChangeUsername = (e) => {
-      const username = e.target.value;
-      setUsername(username);
-    };
-    const onChangeEmail = (e) => {
-    
-      const email = e.target.value;
-      setEmail(email)
-    };
-  
-    const onChangePassword = (e) => {
-    ;
-      const password = e.target.value;
-      setPassword(password);
-    };
-  
-    const onChangeDob = (e) => {
-      const dob = e.target.value;
-      setDob(dob);
-    };
-    const onChangePhone= (e) => {
-      const phone = e.target.value;
-      setPhone(phone);
-    };
-   // const handleChange = (e) => {
-   //   setUser({
-   //      [e.target.name] : [e.target.value]
-   //   })
-   // }
 
-    return (
-        <>
-         <Modal show={props.show1} onHide={props.handleClose1} className='login-form'>
-                     <Modal.Header closeButton>
-                        <Modal.Title>Реєстрація</Modal.Title>
-                     </Modal.Header>
-                     <Modal.Body>
-                     <Form onSubmit={handleSubmit}>
-                        <Form.Group controlId="formBasicFirstName">
-                           <Form.Control type="text" placeholder="Firs tname" value={firstname} onChange={onChangeFirstname} name='firstname'/>
-                        </Form.Group>
-                        <Form.Group controlId="formBasicLastName">
-                           <Form.Control type="text" placeholder="Last name" value={lastname} onChange={onChangeLastname} name='lastname'/>
-                        </Form.Group>
-                        <Form.Group controlId="formBasicName">
-                           <Form.Control type="text" placeholder="User name" value={username} onChange={ onChangeUsername} name='username'/>
-                        </Form.Group>
-                        <Form.Group controlId="formBasicEmail">
-                           <Form.Control type="email" placeholder="Email" value={email}  onChange={onChangeEmail} name='email'/>
-                        </Form.Group>
-         
-                        <Form.Group controlId="formBasicPassword">
-                           <Form.Control type="password" placeholder="Пароль" value={password}  onChange={onChangePassword} name='password' />
-                        </Form.Group>
-                  
-                        <Form.Group controlId="formBasicDateOfBirth">
-                           <Form.Control type="date" value={moment(dob).format("YYYY-MM-DD")}  onChange={onChangeDob} name='dob'/>
-                        </Form.Group>
-                        <Form.Group controlId="formBasicPhone">
-                           <Form.Control placeholder="Номер телефону" value={phone}  onChange={onChangePhone} name='phone'/>
-                        </Form.Group>
-             
-                        <Form.Group controlId="formBasicCheckbox">
-                           <Form.Check type="checkbox" label="Я погоджуюся з умовами користування сайтом" />
-      
-                        </Form.Group>
-                       <div className='flex-form mt-5'>
-                        <Button variant="primary" type="submit" className='btn-form yellow-back'>
-                           Зареєструватись
-                        </Button>
-                        <Button variant="primary" className='btn-form grey-back' onClick={props.handleClose1}>
-                           Скасувати
-                        </Button>
-                        </div>
-                  </Form>
-                     </Modal.Body>
-                     </Modal>
-        </>
-    )
-}
-
-export default SignUpForm
+export default SignUpForm;
---
+++ src/components/Utilities/Utils.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+
+const API_URL = process.env.REACT_APP_URL;;
+
+export const register = (
+    firstname,
+    lastname,
+    username,
+    email,
+    password,
+    dob,
+    phone
+  ) => {
+    return axios.post(API_URL + "/users/register", {
+      firstname,
+      lastname,
+      username,
+      email,
+      password,
+      dob,
+      phone,
+    });
+  };
+
+const login = (email, password) => {
+  return axios
+    .post(API_URL + "signin", {
+      email,
+      password,
+    })
+    .then((response) => {
+      if (response.data.accessToken) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+      }
+
+      return response.data;
+    });
+};
+
+const logout = () => {
+  localStorage.removeItem("user");
+};
+
+const getCurrentUser = () => {
+  return JSON.parse(localStorage.getItem("user"));
+};
+
+export default {
+  
+  login,
+  logout,
+  getCurrentUser,
+};
