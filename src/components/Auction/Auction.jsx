f5a0f422b3aaabcf6c9fb9e745d49144697dac93
OksanaCoder <oksishukh@yahoo.com>
2021-03-13 19:41:20 +0200

    adding style for headers

--- src/components/Auction/Auction.jsx
+++ src/components/Auction/Auction.jsx
@@ -33,8 +33,9 @@ const Auction = () => {
                 <Row className='flex-style padding-filter m-0' style={{background: '#23292D'}}>
                     
                 <div className='heading-style col text-center'>
-                    <h6>Обери</h6>
-                    <h3 className='text-white'>Авто Своєї Мрії</h3>
+                    <div className="blue-line m-auto"></div> 
+                    <h6 className='mt-3'>Обери</h6>
+                    <h3 className='blue-color'>Авто Своєї Мрії</h3>
                 </div>
                
                 </Row>
