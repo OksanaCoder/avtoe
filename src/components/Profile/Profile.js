95883a157a22ca8d5282e89a63b26e6fc4d4fe2c
Oksana Shukh <oksanashukh@Oksanas-MacBook-Pro.local>
2021-04-12 18:46:29 +0300

    updates

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,24 +3,24 @@ import { Container, Col, Row } from 'react-bootstrap'
 import './style.css' 
 
 const Profile = () => {
-    // const [username, setUsername] = useState('')
-    // useEffect(() => {
-    //     console.log('localS name', localStorage.getItem('fullName'))
-    //     setUsername(localStorage.getItem('fullName'))
-    // }, [])
+    const bids = []
     return (
         <Container fluid className='profile p-5'>
             <Row>
                 <Col>
-                 <h2>Welcome,  {localStorage.getItem('fullName')}!</h2>
+                 <h2>Вітаю, {localStorage.getItem('fullName')}</h2>
                 </Col>
             </Row>
           <Row>
               <Col>
-                  <h5 >Your bids: </h5> 
+               
                   {/* I need to show a table with the last bids:
                   ex. car model, time and bid */}
-                
+                {bids.length > 0 ? (
+                    <h5>Твої ставки: </h5> 
+                ) : (
+                    <h5>Поки що немає ставок</h5>
+                )}
               </Col>
           </Row>
         </Container>
