dfc46705b4a1b6da73faf2869ce880dd846a319c
Oksana Shukh <oksanashukh@Oksanas-MacBook-Pro.local>
2021-04-12 18:41:30 +0300

    updates

--- src/components/Magazine/Magazine.jsx
+++ src/components/Magazine/Magazine.jsx
@@ -22,20 +22,9 @@ const Magazine = ({dataMagazine}) => {
                     </div>
                    
                     </Row>
-                    <div className='heading-style p-5'  style={{background: '#262626'}}>                      
-                        <h6 className='blue-line'>Новини</h6>                
-                    </div>
-                    <Row style={{background: '#262626'}} className='p-5 text-white'>
-                   
-                      {dataMagazine
-                        .map((item) => (
-                        
-                            <Col lg={12} md={12} sm={12} key={item.id} >
-                            <ArticleItem dataMagazine={[item]}/> 
-                            </Col>
-                    
-                        ))}
-                    </Row>
+                   <Row className='p-5'>
+                      <a href="https://oksishukh.wixsite.com/my-site-3">Тисни щоб перейти</a>
+                   </Row>
                     
            
             </Container>
