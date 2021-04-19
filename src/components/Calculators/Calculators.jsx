7e210752f927d304cfb04457b0b0eccf89d91cd8
Oksana Shukh <oksanashukh@Oksanas-MacBook-Pro.local>
2021-04-20 02:39:31 +0300

    updates

--- src/components/Calculators/Calculators.jsx
+++ src/components/Calculators/Calculators.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import bank_1 from '../../img/bank-1.png'
 import bank_2 from '../../img/bank-2.png'
-import  bank_3  from '../../img/bank-3.png'
+import bank_3 from '../../img/bank-3.png'
 import bank_4 from '../../img/tac.png'
 import bank_5 from '../../img/hap.png'
 // import bank from '../../img/bank.svg'
@@ -11,50 +11,46 @@ import hapai from '../../img/hapai.png'
 import automoney from '../../img/automoney.png'
 import './style.css'
 
- const Calculators = ({reference}) => {
-    return (
-        <>
-      
-         <Row className='color-light-dark p-5 ' ref={reference}>
-                <div className='heading-style'>
-                    
-                    <h6 className='blue-line'>Авто</h6>
-                    <h3>в Кредит</h3>
-                
-                </div>
-                </Row> 
-          <Container className='color-dark' fluid>
-                <Row className='row-calcs pt-5 pb-4 pl-5 pr-5'>
-                    <Col lg={3} md={6} sm={12}>
-                        <img src={bank_1} className='img-bank'/>
-                    </Col>
-                    <Col lg={3} md={6} sm={12}>
-                        <img src={bank_2} className='img-bank'/>
-                    </Col>
-                    <Col lg={3} md={6} sm={12}>
-                        <img src={bank_3} className='img-bank middle-img'/>
-                    </Col>
-                    <Col lg={3} md={6} sm={12}>
-                        <img src={bank_4} className='img-bank middle-img'/>
-                    </Col>
-                
-                    <Col lg={6} md={6} sm={12}>
-                        <img src={automoney} className='img-bank'/>
-                    </Col>
-                    <Col lg={6} md={6} sm={12}>
-                        <img src={hapai} className='img-bank'/>
-                    </Col>
-                    {/* <Col lg={3} md={6} sm={12}>
+const Calculators = ({ reference }) => {
+  return (
+    <>
+      <Row className="color-light-dark p-5 " ref={reference}>
+        <div className="heading-style">
+          <h6 className="blue-line">Авто</h6>
+          <h3>в Кредит</h3>
+        </div>
+      </Row>
+      <Container className="color-dark" fluid>
+        <Row className="row-calcs pt-5 pb-4 pl-5 pr-5">
+          <Col lg={3} md={6} sm={12} xs={12}>
+            <img src={bank_1} className="img-bank" />
+          </Col>
+          <Col lg={3} md={6} sm={12} xs={12}>
+            <img src={bank_2} className="img-bank" />
+          </Col>
+          <Col lg={3} md={6} sm={12} xs={12}>
+            <img src={bank_3} className="img-bank middle-img" />
+          </Col>
+          <Col lg={3} md={6} sm={12} xs={12}>
+            <img src={bank_4} className="img-bank middle-img" />
+          </Col>
+
+          <Col lg={6} md={6} sm={12} xs={12}>
+            <img src={automoney} className="img-bank" />
+          </Col>
+          <Col lg={6} md={6} sm={12} xs={12}>
+            <img src={hapai} className="img-bank" />
+          </Col>
+          {/* <Col lg={3} md={6} sm={12}>
                         <img src={bank_3} className='img-bank'/>
                     </Col>
                     <Col lg={3} md={6} sm={12}>
                         <img src={bank_1} className='img-bank last'/>
                     </Col> */}
-                </Row>
-          </Container>
-        </>
-
-    )
+        </Row>
+      </Container>
+    </>
+  )
 }
 
 export default Calculators
