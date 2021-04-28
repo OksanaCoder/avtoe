cc3eaef91806e0b3ed4058137a41b23b6149ae2a
code-star <code69023165238739654@star.com>
2021-04-29 00:26:00 +0530

    cleanup

--- src/components/RequestForm/RequestForm.tsx
+++ src/components/RequestForm/RequestForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Modal, Button, Form } from 'react-bootstrap'
 import axios from 'axios'
 import { useHistory } from 'react-router-dom'
@@ -14,31 +14,11 @@ type TFormData = {
 const RequestForm = (props) => {
   const history = useHistory()
 
-  const [username, setUserName] = useState('')
-  const [phone, setPhone] = useState('')
-  const [comment, setModel] = useState('')
-
-  const bodyMessage = `Имя: ${username}
-                     
-                       Сообщение: ${comment}  
-                      
-                       Номер тел: ${phone}`
   // return <div className="pre-line">{bodyMessage}</div>
   // const chat_id = '987210358'
 
-  const API_URL = `https://api.telegram.org/bot1747833143:AAGmm2CnUrkYCyHIdVzEkgJVg2HfNUCba28/sendMessage?chat\_id=987210358&text=${bodyMessage}&parse\_mode=HTML`
-
-  const data = (username, phone, comment) => {
-    return axios.post(API_URL, {
-      username,
-      phone,
-      comment,
-    })
-  }
-
   const {
     handleSubmit,
-    watch,
     formState: { errors },
     control,
   } = useForm<TFormData>({
@@ -49,90 +29,101 @@ const RequestForm = (props) => {
       phone: '',
     },
   })
-  const onSubmit = (data) => console.log(data)
 
-  console.log(watch(), errors)
+  const savePost = (data: TFormData) => {
+    const { username, comment, phone } = data
+    const bodyMessage = `Имя: ${username}
+    
+    Сообщение: ${comment}  
+    
+    Номер тел: ${phone}`
+    const API_URL = `https://api.telegram.org/bot1747833143:AAGmm2CnUrkYCyHIdVzEkgJVg2HfNUCba28/sendMessage?chat\_id=987210358&text=${bodyMessage}&parse\_mode=HTML`
+    return axios.post(API_URL, {
+      username,
+      phone,
+      comment,
+    })
+  }
+
+  const onSubmit = (data: TFormData) => {
+    savePost(data)
+  }
+
   return (
-    <>
-      <Modal show={props.show_form} onHide={props.handleCloseForm} className="login-form">
-        <Modal.Header closeButton>
-          <Modal.Title>Залиште заявку</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <Controller
-              name="username"
-              control={control}
-              rules={{ required: true }}
-              render={({ value, onChange }) => (
-                <Form.Group controlId="username">
-                  <Form.Control
-                    type="text"
-                    placeholder="Ім'я"
-                    value={value}
-                    onChange={onChange}
-                    className="input-style"
-                  />
-                  {errors.username && (
-                    <small style={{ color: 'red' }}>
-                      {JSON.stringify(errors.username)}
-                    </small>
-                  )}
-                </Form.Group>
-              )}
-            />
+    <Modal show={props.show_form} onHide={props.handleCloseForm} className="login-form">
+      <Modal.Header closeButton>
+        <Modal.Title>Залиште заявку</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Controller
+            name="username"
+            control={control}
+            rules={{ required: true }}
+            render={({ value, onChange }) => (
+              <Form.Group controlId="username">
+                <Form.Control
+                  type="text"
+                  placeholder="Ім'я"
+                  value={value}
+                  onChange={onChange}
+                  className="input-style"
+                />
+                {errors.username && (
+                  <small style={{ color: 'red' }}>Username is required.</small>
+                )}
+              </Form.Group>
+            )}
+          />
 
-            <Controller
-              name="comment"
-              control={control}
-              rules={{ required: true }}
-              render={({ value, onChange }) => (
-                <Form.Group controlId="comment">
-                  <Form.Control
-                    type="text"
-                    placeholder="Повідомлення"
-                    value={value}
-                    onChange={onChange}
-                    className="input-style"
-                  />
-                  {errors.comment && (
-                    <small style={{ color: 'red' }}>
-                      {JSON.stringify(errors.comment)}
-                    </small>
-                  )}
-                </Form.Group>
-              )}
-            />
+          <Controller
+            name="comment"
+            control={control}
+            rules={{ required: true }}
+            render={({ value, onChange }) => (
+              <Form.Group controlId="comment">
+                <Form.Control
+                  type="text"
+                  placeholder="Повідомлення"
+                  value={value}
+                  onChange={onChange}
+                  className="input-style"
+                />
+                {errors.comment && (
+                  <small style={{ color: 'red' }}>Comment is required.</small>
+                )}
+              </Form.Group>
+            )}
+          />
 
-            <Controller
-              name="phone"
-              control={control}
-              rules={{ required: true }}
-              render={({ value, onChange }) => (
-                <Form.Group controlId="phone">
-                  <Form.Control
-                    type="text"
-                    placeholder="095 *** ** **"
-                    value={value}
-                    onChange={onChange}
-                    className="input-style"
-                  />
-                  {errors.phone && (
-                    <small style={{ color: 'red' }}>{JSON.stringify(errors.phone)}</small>
-                  )}
-                </Form.Group>
-              )}
-            />
+          <Controller
+            name="phone"
+            control={control}
+            rules={{ required: true }}
+            render={({ value, onChange }) => (
+              <Form.Group controlId="phone">
+                <Form.Control
+                  type="text"
+                  placeholder="095 *** ** **"
+                  value={value}
+                  onChange={onChange}
+                  className="input-style"
+                />
+                {errors.phone && (
+                  <small style={{ color: 'red' }}>Phone is required.</small>
+                )}
+              </Form.Group>
+            )}
+          />
 
-            <div className="text-center mt-5 flex-column">
-              <Button variant="primary" type="submit" className="btn-form yellow-back">
-                Відправити
-              </Button>
-            </div>
-          </form>
-        </Modal.Body>
-      </Modal>
-    </>
+          <div className="text-center mt-5 flex-column">
+            <Button variant="primary" type="submit" className="btn-form yellow-back">
+              Відправити
+            </Button>
+          </div>
+        </form>
+      </Modal.Body>
+    </Modal>
   )
 }
 
